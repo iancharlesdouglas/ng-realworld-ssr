bd3b70623497bc44955c65bb9c4cdf793cdc2968
Ian Douglas <ian@iandouglas.info>
2023-12-13 22:14:04 +0000

    Added pages for pagination; tags

--- src/app/features/home/components/articles/articles.component.ts
+++ src/app/features/home/components/articles/articles.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Article } from '../../../../shared/model/article';
 import { EMPTY, Observable } from 'rxjs';
 import { TagsComponent } from '../tags/tags.component';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgClass } from '@angular/common';
 
 /**
  * List of articles (by feed) incl. set of available tags from which to filter
@@ -10,12 +10,14 @@ import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [AsyncPipe, TagsComponent],
+  imports: [AsyncPipe, NgClass, TagsComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
 })
 export class ArticlesComponent {
   @Input() articles: Observable<Article[]> = EMPTY;
+  @Input() pages: Observable<number[]> = EMPTY;
   @Input() tags: Observable<string[]> = EMPTY;
+  @Input() page: number | undefined;
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { HomeService } from './services/home.service';
 import { Article } from '../../shared/model/article';
 import { ArticlesComponent } from './components/articles/articles.component';
-import { EMPTY, Observable, map,  take, tap } from 'rxjs';
+import { EMPTY, Observable, map, range, tap, toArray } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 /**
@@ -19,17 +19,25 @@ import { AsyncPipe } from '@angular/common';
 export class HomeComponent implements OnInit {
   articles: Observable<Article[]> = EMPTY;
   articlesCount = 0;
+  page = 0;
+  pageSize = 10;
+  pages: Observable<number[]> = EMPTY;
   tags: Observable<string[]> = EMPTY;
 
-  constructor(private readonly homeService: HomeService) {}
+  constructor(private readonly homeService: HomeService, private readonly changeDetector: ChangeDetectorRef) {}
 
   async ngOnInit(): Promise<void> {
+    // const articles$ = this.homeService.getArticles();
+    // const page$ = of(1);
+    // const blah$ = page$.pipe(combineLatestWith(articles$), map(([, articles]) => articles));
+
     this.articles = this.homeService.getArticles().pipe(
       tap((response) => {
-        this.articlesCount = response.articlesCount;
+        this.pages = range(0, Math.floor((response.articlesCount - 1) / this.pageSize) + 1)
+          .pipe(toArray());
+        this.changeDetector.detectChanges();
       }),
-      map((response) => response.articles),
-      take(5)
+      map((response) => response.articles.slice(this.page * this.pageSize, (this.page + 1) * this.pageSize)),
     );
     this.tags = this.homeService.getTags().pipe(map((response) => response.tags));
   }
