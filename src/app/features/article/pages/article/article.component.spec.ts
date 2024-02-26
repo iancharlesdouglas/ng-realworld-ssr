091d51eb27bbf41d563f14a8b22bc3113e692e4c
Ian Douglas <ian@iandouglas.info>
2024-02-26 08:05:24 +0000

    Article retrieval test - WIP

--- src/app/features/article/pages/article/article.component.spec.ts
+++ src/app/features/article/pages/article/article.component.spec.ts
@@ -1,22 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpHandler } from '@angular/common/http';
-import { from } from 'rxjs';
+import { from, of } from 'rxjs';
 import { mockHttpClient, mockHttpHandler } from '../../../../shared/tests/mock-http-client';
 import { ArticleComponent } from './article.component';
+import { User } from '../../../../shared/model/user';
+import { StateService } from '../../../../shared/services/state/state.service';
+import { vi } from 'vitest';
+import { ArticleService } from '../../../../shared/services/article.service';
+import { environment } from '../../../../../environments/environment';
+import { Article } from '../../../../shared/model/article';
+import { Comment } from '../../../../shared/model/comment';
 
 describe('ArticleComponent', () => {
   let component: ArticleComponent;
   let fixture: ComponentFixture<ArticleComponent>;
 
-  beforeEach(async () => {
+  const setUpComponent = async (slug = '') => {
+    const user: User = { username: 'x', email: 'x@y.com', token: 'some_token' };
+    const stateService = new StateService();
+    stateService.setUser(user);
+    const router = {
+      navigate: vi.fn(),
+      events: of([]),
+      createUrlTree: vi.fn(),
+      serializeUrl: vi.fn()
+    };
     await TestBed.configureTestingModule({
       imports: [ArticleComponent],
       providers: [
-        { provide: ActivatedRoute, useValue: {params: from([{id: 'x'}])} },
+        { provide: ActivatedRoute, useValue: {params: from([{id: slug}])} },
+        { provide: StateService, useValue: stateService },
         { provide: HttpClient, useValue: mockHttpClient },
         { provide: HttpHandler, useValue: mockHttpHandler },
+        ArticleService,
+        { provide: Router, useValue: router }
       ]
     })
     .compileComponents();
@@ -24,15 +42,56 @@ describe('ArticleComponent', () => {
     fixture = TestBed.createComponent(ArticleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  };
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should retrieve an article with its comments', async () => {
+    const slug = '123';
+    const article: Article = {
+      slug,
+      title: 'Lorem Ipsum',
+      description: 'Lorem ipsum tacitus atsale',
+      body: 'Lorem ipsum tamantes tacistus ipto decitus maxi.',
+      tagList: [],
+      author: {
+        username: 'joebloggs25',
+        following: false
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      favorited: false,
+      favoritesCount: 100
+    };
+    const comments: Comment[] = [
+      {
+        id: 1_000_001,
+        body: 'Great article!',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        author: {
+          username: 'jimtibbs22',
+          following: false
+        }
+      }
+    ];
+    mockHttpClient.get = vi.fn().mockImplementation(url => {
+      if (url === `${environment.remoteApiHost}/api/articles/${slug}`) {
+        return of({article});
+      } else {
+        return of({comments});
+      }
+    });
 
-  // it('should retrieve an article with its comments', () => {
-  //   throw 'not implemented';
-  // });
+    await setUpComponent(slug);
+
+    const bodyParagraph = fixture.nativeElement.querySelector('.article-content') as HTMLParagraphElement;
+    expect(bodyParagraph.textContent).toEqual(article.body);
+
+    const commentsSection = fixture.nativeElement.querySelector('.comments') as HTMLDivElement;
+    expect(commentsSection).toBeDefined();
+    // const commentParagraphs = fixture.nativeElement.querySelectorAll('.comment p.card-text') as HTMLParagraphElement[];
+    // expect(commentParagraphs.length).toEqual(comments.length);
+    // expect(commentParagraphs[0].textContent).toEqual(comments[0].body);
+  });
 
   // it('should favorite an article when requested', () => {
   //   throw 'not implemented';
